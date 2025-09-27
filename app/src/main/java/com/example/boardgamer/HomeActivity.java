a5362bc999f1a2070024a1b0abdef3e64cd1c028
Selina Collini <selina.collini@iu-study.org>
2025-09-27 11:53:33 +0200

    last changes in home activity, also added option to change pages with bottom nav menu - thesefore added MessagesActivity.java and XML

--- app/src/main/java/com/example/boardgamer/HomeActivity.java
+++ app/src/main/java/com/example/boardgamer/HomeActivity.java
@@ -1,12 +1,16 @@
 package com.example.boardgamer;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -15,10 +19,28 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.activity_home), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+
+        //@Eduard: for bottom nav menu - changing pages - need to add that on every page which includes bottom nav
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int id = item.getItemId();
+            if (id == R.id.home) {
+                return true;
+            } else if (id == R.id.messages) {
+                startActivity(new Intent(this, MessagesActivity.class));
+                return true;
+            } else if (id == R.id.settings) {
+                return true;
+            }
+            return false;
         });
     }
 }
